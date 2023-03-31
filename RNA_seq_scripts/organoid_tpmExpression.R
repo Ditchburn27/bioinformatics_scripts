25428fd2a20a5ec48b859ea092fcd65c732873d9
Leighton <layditch@icloud.com>
2023-03-31 14:53:48 +0800

    Added a script to plot the tpm expression of genes in organoids, using the bulk RNA-seq data set

---
+++ RNA_seq_scripts/organoid_tpmExpression.R
@@ -0,0 +1,24 @@
+# Use this script to have a quick look at the expression of a gene of 
+# interest in organoids, using the bulk RNA-seq dataset from my honours project.
+
+library(tidyverse)
+library(ggplot2)
+
+# Read in data table with TPM values and hgnc gene symbols
+tpm_results <- read.table(
+  file = "organoid_bulkRNAseq_tpmExpression_geneSymbol_table.txt", sep = ","
+  , header = TRUE)
+
+# Function to plot a histogram of TPM values in all organoid conditions
+plot_tpms <- function(gene = "") {
+  expression <- subset(tpm_results, tpm_results$hgnc_symbol == gene)
+  t.expression <- pivot_longer(expression, cols = 1:24,
+                               names_to = 'sample', values_to = 'tpm')
+  t.expression <- t.expression[, -1]
+  
+  return(ggplot(t.expression, aes(x = sample, y = tpm)) + 
+           geom_col() + ggtitle(paste(
+             gene, "expression in organoids", sep = " ")))
+}
+
+# Example: plot_tpms(gene = "SMAD3")
