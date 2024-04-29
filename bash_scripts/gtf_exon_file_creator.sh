bdb06ae9410658d9e93bc6184a31d4351dd64561
Leighton <layditch@icloud.com>
2024-04-29 11:55:33 +0800

    Updated gtf exon file creator for cellranger provided gtf files

--- bash_scripts/gtf_exon_file_creator.sh
+++ bash_scripts/gtf_exon_file_creator.sh
@@ -6,8 +6,8 @@ output_gtf_name=$2
 awk -F "\t" '$3=="exon"' $input_gtf > tmp1.gtf
 tr ';' '\t' < tmp1.gtf > tmp2.gtf
 awk -F'"' 'BEGIN {OFS="\t"} {$1=$1; gsub(/ +/, "\t"); gsub(/"/, "", $2); print}' tmp2.gtf > tmp3.gtf
-awk -F'\t' 'BEGIN {OFS="\t"} {$9 = $9 "\t" substr($11, 1, length($11)-2) "-" substr($44, 1, length($44)-2); print}' tmp3.gtf > tmp4.gtf
-cut -f 1-10,19-28 tmp4.gtf > tmp5.gtf
+awk -F'\t' 'BEGIN {OFS="\t"} {$9 = $9 "\t" substr($11, 1, length($11)) "-" substr($54, 1, length($54)); print}' tmp3.gtf > tmp4.gtf
+cut -f 1-10,30-37 tmp4.gtf > tmp5.gtf
 awk -v field=10 'BEGIN {OFS="\t"} {if (NF >= field) $field = "\"" $field "\""; print}' tmp5.gtf > tmp6.gtf
 awk -v field=12 'BEGIN {OFS="\t"} {if (NF >= field) $field = "\"" $field "\""; print}' tmp6.gtf > tmp7.gtf
 awk -v field=14 'BEGIN {OFS="\t"} {if (NF >= field) $field = "\"" $field "\""; print}' tmp7.gtf > tmp8.gtf
