f36b39b2e1184b4bba85037fb33b1f3f406ea9bf
Leighton <layditch@icloud.com>
2024-05-27 10:42:28 +0800

    Updated script for making anndata object. Includes adding tile matrix & selecting features

--- snATAC_analysis_scripts/preprocessing/3_make_anndata_from_merged_fragments.py
+++ snATAC_analysis_scripts/preprocessing/3_make_anndata_from_merged_fragments.py
@@ -1,5 +1,6 @@
 import snapatac2 as snap
 import argparse
+import os
 
 ######################################################
 # CLI inputs
@@ -17,8 +18,24 @@ fragment_file = args.fragment_file
 
 ######################################################
 # Import fragment file and make anndata object. 
+print("Importing fragment file...")
 adata = snap.pp.import_data(fragment_file,
                             chrom_sizes=snap.genome.GRCh38,
                             sorted_by_barcode=False)
 
-adata.write('braindev_hg38_snATAC_raw.h5ad')
+######################################################
+# Calculate metrics & preprocess
+print("Calculating TSSE scores...")
+snap.metrics.tsse(adata, snap.genome.GRCh38)
+
+print("Filtering cells...")
+snap.pp.filter_cells(adata, min_counts=2000, min_tsse=None)
+
+print("Adding Tile Matrix...")
+snap.pp.add_tile_matrix(adata, bin_size=5000, counting_strategy='fragment')
+
+print("Selecting features...")
+blacklist = '/group/ll005/reference/bowtie2_hg38/blacklist/hg38-blacklist.v2.bed'
+snap.pp.select_features(adata, n_features=500000, blacklist=blacklist)
+
+adata.write('braindev_hg38_snATAC_tile_matrix.h5ad')
