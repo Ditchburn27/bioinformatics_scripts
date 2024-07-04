f926dce948e63180eaf3a6a813f784a7ea24a552
Leighton <layditch@icloud.com>
2024-07-04 17:09:28 +0800

    Added function for counting & removing doublets

--- snATAC_analysis_scripts/snatac_utils.py
+++ snATAC_analysis_scripts/snatac_utils.py
@@ -7,6 +7,9 @@ from pybedtools import BedTool
 from multiprocessing import Pool, cpu_count
 from tqdm import tqdm
 import pandas as pd
+import matplotlib.cm as cm
+from matplotlib.pyplot import rc_context
+from scipy.stats import pearsonr
 
 #####################################
 ## Plot cell counts of specified obs for 
@@ -524,3 +527,45 @@ def scatter_plot_multiple_y(adata, x_var, y_vars):
 
     # Show plot
     plt.show()
+
+#####################################
+# Function for counting number of doublets
+# and removing doublets
+def count_remove_doublets(adata=adata, groupby='library_id'):
+    """
+    Count and remove doublets from an AnnData object.
+
+    This function identifies and counts doublets in the `adata` object for each group specified
+    by the `groupby` parameter, prints the number of doublets for each group, the total number
+    of doublets, and removes these doublets from the dataset.
+
+    Parameters:
+    ----------
+    adata : AnnData
+        The AnnData object containing single-cell data. The `.obs` attribute should have a 
+        boolean column 'predicted_doublet' that indicates whether each cell is a predicted doublet.
+    groupby : str, optional (default: 'library_id')
+        The column name in `adata.obs` by which to group the data and count doublets.
+
+    Returns:
+    -------
+    None
+        The function modifies the input `adata` in place by removing the doublets.
+        
+    Notes:
+    -----
+    - Ensure that `adata` contains a boolean column named 'predicted_doublet' in its `.obs` attribute.
+    - The function prints the number of doublets for each group and the total number of doublets.
+    - The function filters out the doublets from the dataset.
+    """
+    # Print number of doublets for each library
+    doublet_counts = adata.obs.groupby(groupby)['predicted_doublet'].sum()
+    print('Doublets identified for each sample:')
+    for library_id, counts in doublet_counts.items():
+        print(f'{groupby}: {library_id}, number of doublets = {counts}')
+    total = doublet_counts.sum()
+    print(f'Total number of doublets = {total}')
+    # Remove doublets from dataset
+    dub_mk = adata.obs['predicted_doublet']
+    adata = adata[~dub_mk]
+    return
